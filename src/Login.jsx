6c28d4515f846927d699e19cbf2e71e6014cd809
Idris <ianleraldd@gmail.com>
2024-12-06 01:25:51 +0800

    made the design better

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,10 +3,10 @@ import Logo from './assets/Logo.png'
 function Login(){
     return(
         <div className="h-[100vh] w-[100vw] flex">
-            <div className="loginPlanet w-[65%] h-[100%]">
-                <h1 className="text-white px-[25px] text-[50px] bg-black font-agdasima">LOG IN</h1>
+            <div className="loginPlanet w-[60%] h-[100%]">
+                <h1 className="text-white px-[25px] text-[50px] font-agdasima">LOG IN</h1>
             </div>
-            <div className="w-[35%] h-[100%] bg-[#0D0D0E] flex items-center justify-center flex-col gap-[10%]">
+            <div className="w-[40%] h-[100%] bg-[#0D0D0E] flex items-center justify-start pt-[5%] flex-col gap-[30%]">
                 <img className='w-[75%]' src={Logo} alt="" />
                 <button className='px-10 py-2 text-3xl bg-[#915472] font-inria text-white rounded-[20px] login transition-all ease-in-out duration-[800ms]'>Log In With <br />Google</button>
             </div>
