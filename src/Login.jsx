665c0978ff778f2e206932a3a14b5d1a64477926
Idris <ianleraldd@gmail.com>
2024-12-06 01:08:45 +0800

    Finished Log In Page

---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+import Logo from './assets/Logo.png'
+
+function Login(){
+    return(
+        <div className="h-[100vh] w-[100vw] flex">
+            <div className="loginPlanet w-[65%] h-[100%]">
+                <h1 className="text-white px-[25px] text-[50px] bg-black font-agdasima">LOG IN</h1>
+            </div>
+            <div className="w-[35%] h-[100%] bg-[#0D0D0E] flex items-center justify-center flex-col gap-[10%]">
+                <img className='w-[75%]' src={Logo} alt="" />
+                <button className='px-10 py-2 text-3xl bg-[#915472] font-inria text-white rounded-[20px] login transition-all ease-in-out duration-[800ms]'>Log In With <br />Google</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
